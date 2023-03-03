ae63461c36a78f39c9d7d4cf35696d94fb7015ba
RashedAL <rashed.alemad@aiegroup.org>
2023-03-03 22:58:11 +0400

    Currency Converter app get data from api cashe them presentation layer is not yet completed

--- app/src/main/java/com/example/flowexample/veracity/presentation/viewmodels/VeracityViewModel.kt
+++ app/src/main/java/com/example/flowexample/veracity/presentation/viewmodels/VeracityViewModel.kt
@@ -5,37 +5,44 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.flowexample.core.base.BaseViewModel
 import com.example.flowexample.veracity.data.CurrencyRepostiory
+import com.example.flowexample.veracity.models.Currency
 import com.example.flowexample.veracity.models.USDResponse
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.reflect.full.declaredMemberProperties
 
 class VeracityViewModel  @Inject constructor (private val currencyRepostiory: CurrencyRepostiory):BaseViewModel() {
     var response: MutableLiveData<USDResponse> = MutableLiveData()
 
 
-    fun clikced(){
-        Log.d("viemodel", "clikced: ${currencyRepostiory.test()}")
-viewModelScope.launch {
-
-    response = currencyRepostiory.getUSDRates()
-
-    Log.d("viemodel", "clikced: in view model  ${response.value}")
 
 
-}
-        Log.d("viemodel", "clikced: after ")
-
-    }
 
     fun getUSDRates() {
-        Log.d("viemodel", "clikced: ${currencyRepostiory.test()}")
         viewModelScope.launch {
 
             response = currencyRepostiory.getUSDRates()
 
-            Log.d("viemodel", "clikced: in view model  ${response.value}")
 
 
         }    }
+
+    fun formulateData(usdResponse: USDResponse) :MutableList<Currency>{
+
+        var currencies = mutableListOf<Currency>()
+        usdResponse.rates
+        usdResponse.rates?.USD?.let { Currency("USD", it) }?.let { currencies.add(it) }
+        usdResponse.rates?.AED?.let { Currency("AED", it) }?.let { currencies.add(it) }
+        usdResponse.rates?.AFN?.let { Currency("AFN", it) }?.let { currencies.add(it) }
+        usdResponse.rates?.ALL?.let { Currency("ALL", it) }?.let { currencies.add(it) }
+        usdResponse.rates?.AMD?.let { Currency("AMD", it) }?.let { currencies.add(it) }
+        usdResponse.rates?.ANG?.let { Currency("ANG", it) }?.let { currencies.add(it) }
+        usdResponse.rates?.AOA?.let { Currency("AOA", it) }?.let { currencies.add(it) }
+        usdResponse.rates?.ARS?.let { Currency("ARS", it) }?.let { currencies.add(it) }
+        usdResponse.rates?.AUD?.let { Currency("AUD", it) }?.let { currencies.add(it) }
+        usdResponse.rates?.AWG?.let { Currency("AWG", it) }?.let { currencies.add(it) }
+return currencies
+
+    }
 
 }
